6a60501d58438b389ccdaa62b22b8600eda4cea3
WayanAndika <andikabuana45@gmail.com>
2023-06-20 03:58:11 -0700

    Update Index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require("express");
 const port=3000;
 const app=express();
 app.get("/",(req,res)=>{
-    res.send("Hello World");
+    res.send("Hai ini baru");
 })
 
 app.listen(port,()=>{
