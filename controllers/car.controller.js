476b986c41b7d91dd6db8669f68759f9f334d26f
WayanAndika <andikabuana45@gmail.com>
2023-06-21 09:19:30 -0700

    Menambahkan fitur Edit dan Hapus Data

--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -31,6 +31,39 @@ class carController{
         });
         res.redirect("/cars");
     }
+    static async editCar(req,res){
+        const result = await prisma.car.findUnique({
+            where:{
+                id:Number(req.params.id)
+            }
+        });
+        res.render("pages/cars/edit",{car:result});
+    }
+    static async updateCar(req,res){
+        await prisma.car.update({
+            where:{
+                id:Number(req.params.id)
+            },
+            data:{
+                name:req.body.name,
+                merk:req.body.merk,
+                qty:Number(req.body.qty),
+                desc:req.body.desc,
+                price:Number(req.body.price),
+                img : req.body.img,
+                available:req.body.available === "1" ? true : false
+            }
+        });
+        res.redirect("/cars");
+    }
+    static async deleteCar(req,res){
+        await prisma.car.delete({
+            where:{
+                id:Number(req.params.id)
+            }
+        });
+        res.redirect("/cars");
+    }
 }
 
 module.exports= carController;
--- routers/cars.router.js
+++ routers/cars.router.js
@@ -7,6 +7,9 @@ const router=Router();
 router.get("/cars",carController.listCars);
 router.get("/car/create",carController.addCar);
 router.post("/car",carController.storeCar);
+router.get("/car/:id/edit",carController.editCar);
+router.post("/car/:id/edit",carController.updateCar);
+router.post("/car/:id/delete",carController.deleteCar);
 router.get("/car/:id",carController.detailCar);
 
 
